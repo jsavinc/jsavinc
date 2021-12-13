4d6aa5df630a3bc74d12ba6a4627b2d498f9c5f4
jsavinc <j.savinc@napier.ac.uk>
2021-12-13 12:53:29 +0000

    addded .nojekyll to the source repo also to stop github from trying to build the website

--- R/build2.R
+++ R/build2.R
@@ -1,5 +1,6 @@
 # An optional custom script to run after Hugo builds your site.
 # You can delete it if you do not need it.
 
-# create a file in the /public dir to stop jekyll
+# create files to stop jekyll
+file.create("./.nojekyll")
 file.create("./public/.nojekyll")
